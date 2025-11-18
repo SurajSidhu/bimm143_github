13325bf7757d0b6e33b38cfd2218f3004208b666
Suraj Sidhu <s4sidhu@ucsd.edu>
2025-11-18 12:00:49 -0800

    folders commited

---
+++ Class04_files/Class04.R
@@ -0,0 +1,10 @@
+#' ---
+#' title: "Class 4 Example"
+#' author: "Suraj Sidhu (A18512793)"
+#' date: "October 9th, 2025"
+#' ---
+x <- 1:50
+plot(x,sin(x), typ="l", col="forestgreen", lwd=3)
+
+x <- 1:10
+x[-1]
